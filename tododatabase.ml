f439fafe3efd7267dd2b2aac2980fa85ff811ae9
Till Crueger <till@Zion.(none)>
2010-10-21 14:37:42 +0200

    Added output function for todos and databases

--- tododatabase.ml
+++ tododatabase.ml
@@ -25,10 +25,31 @@ type 'a todo = tododata with sexp
 
 type 'a todolist = ('a todo) list with sexp
 
-let add_todo todo todolist    = todo :: todolist
-let delete_todo todo todolist = List.filter (fun x -> x != todo) todolist
+type 'a database = 'a todolist ref
 
+let make_database () = ref []
 
+let make_open_todo duetime name = {duetime = duetime;subject=name}
+
+let add_todo todo database    = database := todo :: !database
+let delete_todo todo database = List.filter (fun x -> x != todo) database
+
+let string_of_repetition =
+	function
+		Daily -> "Täglich"
+	| Weekly weekday -> Printf.sprintf "jeden %s" (Date.string_of_weekday weekday)
+	| Monthly day    -> Printf.sprintf "jeden %i." day
+	| Weekdays       -> "wochentags"
+	| Weekends       -> "am wochenende"
+
+let string_of_todotime =
+	function
+		Repeated repetition -> string_of_repetition repetition 
+  | Single   date       -> 
+					Printf.sprintf "bis %s" (if Date.is_today date then "heute" else Date.string_of_date date)
+
+let string_of_todo todo =
+	Printf.sprintf "%s\t %s" todo.subject (string_of_todotime todo.duetime)
 
 
               
