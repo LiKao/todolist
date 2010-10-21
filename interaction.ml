474787ea1225d11748914ee2018626862fa35e2b
Till Crueger <till@Zion.(none)>
2010-10-21 15:28:28 +0200

    added more interaction functions

--- interaction.ml
+++ interaction.ml
@@ -1,11 +1,11 @@
 open Printf
 
-exception NoSelection
+exception No_selection
 
 type 'a choice = {name  : string;
                   value : 'a}
 
-type shortcut = char
+type shortcut = string
 							
 type 'a menu = {title   : string;
                 choices : (shortcut * ('a choice)) list}
@@ -17,20 +17,22 @@ let (|->) (shortcut,name,menustub) value = {title = menustub.title;
 																					   [(shortcut,{name = name;value = value})]}
 let (|%|) menustub shortcut = (shortcut,menustub)																						
 		
-
-let display_menu menu =
+let make_choices choices title =
+	let rec loop i items =
+		match items with
+			(value,name) :: items -> (sprintf "%i" i,{name = name;value=value}) :: (loop (i+1) items)
+		| [] -> []
+	in
+	{title = title; choices = loop 1 choices} 
+		
+let display_choice menu =
 	printf "\n\n%s\n" menu.title;
 	printf "------------------------\n";
-	List.iter (fun (shortcut,choice) -> printf "%c: %s\n" shortcut choice.name) menu.choices;
+	List.iter (fun (shortcut,choice) -> printf "%s:\t%s\n" shortcut choice.name) menu.choices;
 	flush stdout;
 	let rec loop () =
-		let selection =
-			try
-				let line = input_line stdin in
-				line.[0]
-			with Invalid_argument _ ->
-				raise NoSelection
-		in
+		let selection = input_line stdin in
+		if String.length selection = 0 then raise No_selection;
 		try begin
 			let res = List.assoc selection menu.choices in 
 			res.value
@@ -42,4 +44,8 @@ let display_menu menu =
 				loop ()
 			end
 	in loop ()
+
+let rec display_menu menu =
+	try display_choice menu
+	with No_selection -> display_menu menu
 	
