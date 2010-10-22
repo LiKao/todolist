abced06732cbb95b9f38aeae989f9019cc520af1
Till Crueger <till@Zion.(none)>
2010-10-22 11:59:03 +0200

    Added functions to close or drop todos

--- tododatabase.ml
+++ tododatabase.ml
@@ -1,19 +1,41 @@
 TYPE_CONV_PATH "Current.Module.Name"
 
-type repetition = Daily
-                | Weekly of Date.weekday
-								| Monthly of Date.dayofmonth
-								| Weekdays
-								| Weekends
-								with sexp
+type repetition = 
+	| Daily
+	| Weekly of Date.weekday
+	| Monthly of Date.dayofmonth
+	| Weekdays
+	| Weekends
+	with sexp
 
-type todotime = Repeated of repetition
-              | Single   of Date.date
-							with sexp
+type todotime = 
+	| Repeated of repetition
+  | Single   of Date.date
+	with sexp
+	
+let is_repeated =
+	function
+		| Repeated _ -> true
+		| Single   _ -> false
+	
+type todostate = 
+	| Pending
+	| Closed     of Date.date
+	| Unfinished of Date.date
+	with sexp
+	
+let is_open =
+	function
+		| Pending       -> true
+		| Closed      _ -> false
+		| Unfinished  _ -> false
+							
 
-type tododata = {duetime : todotime;
-                 subject : string}
-								with sexp
+type tododata = 
+	{duetime : todotime;
+   subject : string;
+	 state   : todostate}
+	with sexp
 
 																								
 type todolist = tododata list with sexp
@@ -22,10 +44,13 @@ type database = todolist ref
 
 let make_database () = ref []
 
-let make_open_todo duetime name = {duetime = duetime;subject=name}
+let make_open_todo duetime name = {duetime = duetime;subject=name;state=Pending}
 
 let add_todo todo database    = database := todo :: !database
 let delete_todo todo database = List.filter (fun x -> x != todo) database
+
+let close_todo todo date = {todo with state=Closed date}
+let drop_todo  todo date = {todo with state=Unfinished date}
 
 let string_of_repetition =
 	function
@@ -39,7 +64,7 @@ let string_of_todotime =
 	function
 		Repeated repetition -> string_of_repetition repetition 
   | Single   date       -> 
-					Printf.sprintf "bis %s" (if Date.is_today date then "heute" else Date.string_of_date date)
+		  Printf.sprintf "bis %s" (if Date.is_today date then "heute" else Date.string_of_date date)
 
 let string_of_todo todo =
 	Printf.sprintf "%s\t %s" todo.subject (string_of_todotime todo.duetime)
