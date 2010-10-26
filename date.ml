5a1a828a858dfeee853ecd0105a28e85c5d5059a
Till Crueger <till@Zion.(none)>
2010-10-26 10:52:35 +0200

    Added a function to get the number of days in a month

--- date.ml
+++ date.ml
@@ -54,6 +54,22 @@ let string_of_month =
 	|	November -> "November"
 	|	December -> "Dezember"
 
+
+let days_of_month month year =
+	match month with
+		January -> 31
+	|	February -> if is_leapyear year then 29 else 28
+	| March -> 31
+	|	April -> 30
+	|	May -> 31
+	|	June -> 30
+	|	July -> 31
+	|	August -> 31
+	|	September -> 30
+	|	Oktober -> 31
+	|	November -> 30
+	|	December -> 31 	
+
 let months = [|
 			January;
       February;
