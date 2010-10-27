0192b17c4c9354c58c61d3b363d185cf68c4a4c0
Till Crueger <Till.Crueger@gmx.net>
2010-10-27 14:00:43 +0200

    Added a wider variety of range queries to the range module

--- range.ml
+++ range.ml
@@ -12,11 +12,20 @@ module type S =
 sig
 	type key
 	type 'a t
-	type range = {start : key; finish : key}
+	type interval = {start : key; finish : key} 
+	type range = 
+		| Nothing
+		| At      of key
+		| Before  of key
+		| After   of key
+		| Between of interval 
 	val empty   : 'a t
 	val add     : 'a t -> key -> 'a -> 'a t
 	val find    : 'a t -> key -> 'a list
-	val find_range : 'a t -> range -> 'a list
+	val find_before : 'a t -> key -> 'a list
+	val find_after  : 'a t -> key -> 'a list
+	val find_between  : 'a t -> interval -> 'a list
+	val find_range  : 'a t -> range -> 'a list
 	val to_list : 'a t -> 'a list
 	val from_list : ('a -> key) -> 'a list -> 'a t
 end
@@ -26,7 +35,13 @@ struct
 	type key = Data.t
 	type 'a leaf = {key : key; content : 'a}
 	type 'a t = 'a leaf list 
-	type range = {start : key; finish : key}
+	type interval = {start : key; finish : key}
+	type range =
+		| Nothing
+		| At      of key
+		| Before  of key
+		| After   of key
+		| Between of interval 
 	
 	let compare = Data.compare
 	
@@ -58,12 +73,37 @@ struct
 						[]
 			|  [] -> []
 		in loop ls
-		
-	let find_range ls range =
+
+	let find_before ls key =
+		let rec loop =
+			function
+				node :: nodes ->
+					let comparison = compare node.key key in
+					if comparison <= 0 then
+						List.map (fun leaf -> leaf.content) ls
+					else
+						loop nodes
+			| [] -> []
+		in loop ls
+	
+	let find_after ls key =
+		let rec loop =
+			function
+				node :: nodes ->
+					let comparison = compare node.key key in
+					if comparison > 0 then
+						node.content :: (loop nodes)
+					else
+						[]
+			| [] -> []
+		in
+		loop ls		
+						
+	let find_between ls interval =
 		let rec loop1 = 
 			function
 				node :: nodes ->
-					let comparison = compare node.key range.start in
+					let comparison = compare node.key interval.start in
 					if comparison > 0 then
 						node.content :: (loop1 nodes)
 					else
@@ -73,19 +113,27 @@ struct
 		let rec loop2 =
 			function
 				node :: nodes -> 
-					let comparison = compare node.key range.finish in
+					let comparison = compare node.key interval.finish in
 					if comparison <= 0 then
 						loop1 (node :: nodes)
 					else
 						loop2 nodes
 			| [] -> []
 		in loop2 ls
+	
+	let find_range ls =
+		function
+		| Nothing          -> [] 
+		| At      key      -> find ls key 
+		|	Before  key      -> find_before ls key
+		| After   key      -> find_after ls key
+		| Between interval -> find_between ls interval 
 		
-		let to_list ls =
-			List.map (fun leaf -> leaf.content) ls		
-			
-		let from_list get_key =
-			List.fold_left 
-				(fun ls value -> add ls (get_key value) value)
-				empty	
+	let to_list ls =
+		List.map (fun leaf -> leaf.content) ls		
+		
+	let from_list get_key =
+		List.fold_left 
+			(fun ls value -> add ls (get_key value) value)
+			empty	
 end
