bdd2baf655f0cc87f061e09da747f9bd2376ce8d
Till Crueger <Till.Crueger@gmx.net>
2010-11-11 14:28:31 +0100

    Added classes to table elements of tododata

--- services/todoservice.ml
+++ services/todoservice.ml
@@ -11,11 +11,13 @@ let show_todos db date =
 		let subject = Todo.get_subject todo in
 		let duetime = Todo.string_of_todotime (Todo.get_duetime todo) in
 		Basic_Tables.tr 
-					(Basic_Tables.td [div [pcdata subject]]) [Basic_Tables.td [div [pcdata duetime]]]
+					(Basic_Tables.td [div ~a:[a_class ["todosubject"]] [pcdata subject]]) 
+					 [Basic_Tables.td ~a:[a_class ["todotime"]] [div [pcdata duetime]]]
 	in  
 	Basic_Tables.table 
 		(Basic_Tables.tr 
-			(Basic_Tables.td [h6 [pcdata "Betreff"]]) [Basic_Tables.td [h6 [pcdata "Datum"]]]
+			(Basic_Tables.td [div ~a:[a_class ["tableheader"]] [pcdata "Betreff"]]) 
+			[Basic_Tables.td [div ~a:[a_class ["tableheader"]] [pcdata "Datum"]]]
 		) 
 		(List.map printer active_todos)
 		
